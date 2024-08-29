4c3b7f6dbfd474a8dc05e4dd7f3a63a45ea667d4
rhouzf <rhouzali.f@gmail.com>
2024-08-29 04:09:58 +0100

    Correction des chemins dans index.html pour le déploiement GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/react-3d-model/docs/',
+  base: '/react-3d-model/',
   plugins: [react()],
   build: {
     outDir: 'docs/assets', // Change this to your desired output directory
